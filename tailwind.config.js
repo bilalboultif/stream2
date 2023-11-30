cdbaf2c54ca7b485aaefc546cc2b821c058bffb9
bilalboultif <80066430+bilalboultif@users.noreply.github.com>
2023-11-30 00:08:10 -0500

    Initialize feat/tailwind-setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx, js}", "./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  important: "#root",
+  plugins: [],
+};
